9966e54cd8f64ff16ed98f9032a5d60b4c743967
ducwp <ducwebvn@gmail.com>
2022-04-26 09:17:02 +0700

    ok

--- vnexpress.js
+++ vnexpress.js
@@ -1,2 +1,5 @@
 document.querySelector("a.logo img").src = "https://ducwp.github.io/baosach/vnexpress.svg";
 document.querySelector("a.logo img").height = "40";
+
+document.querySelector("a.img_logo img").src = "https://ducwp.github.io/baosach/vnexpress.svg";
+document.querySelector("a.img_logo img").height = "40";
